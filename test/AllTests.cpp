804e09ad93eac9d97389d0c1906979b33afe7427
David Fink <davidfink314@gmail.com>
2022-02-24 06:35:01 -0500

    rename tcountWithSmallPrimeFactors.cpp -> ttCountWithSmallPrimeFactors.cpp since git won't recognize a caps-only filename change

--- test/AllTests.cpp
+++ test/AllTests.cpp
@@ -4,7 +4,7 @@
 #include "tCircleIterator.hpp"
 #include "tClass.hpp"
 #include "tContainerTransformer.hpp"
-#include "tcountWithSmallPrimeFactors.hpp"
+#include "ttCountWithSmallPrimeFactors.hpp"
 #include "tDFS.hpp"
 #include "tFraction.hpp"
 #include "tGraphReducer.hpp"
