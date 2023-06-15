c9ce60ef6d0fa37a739b87f0b4db554a9ebdd28e
David Fink <davidfink314@gmail.com>
2023-06-14 23:39:23 -0400

    Add zero(example_value)

--- Constants/Zero.hpp
+++ Constants/Zero.hpp
@@ -6,9 +6,17 @@ class Zero {
     static inline T get() {
 	return T(0);
     }
+    static inline T get(T const&) {
+	return get();
+    }
 };
 
 template<class T>
 T zero() {
     return Zero<T>::get();
+}
+
+template<class T>
+T zero(T const& t) {
+    return Zero<T>::get(t);
 }
